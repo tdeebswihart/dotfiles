ad83fadab732dad450421aba7d4aa1e07e5959dd
Tim Swihart <chronoslynx@gmail.com>
2013-03-25 15:19:45 -0400

    adding other config files

--- aliases.sh
+++ aliases.sh
@@ -3,5 +3,6 @@ alias irb=pry
 alias rm=trash
 alias make430="PATH=`brew --prefix llvm-msp430`/bin:$PATH make"
 alias mspmake="PATH=`brew --prefix llvm-msp430`/bin:$PATH make"
+alias timestamp='gawk "{now=strftime(\"%F %T \"); print now \$0; fflush(); }"'
 
 #eval "$(hub alias -s)"
