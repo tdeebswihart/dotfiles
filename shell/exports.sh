9d708e765a654031692f68914fd927450ea9e556
Tim Swihart <chronoslynx@gmail.com>
2013-04-11 12:00:53 -0400

    changed blogroot/blogname to match github's move to io

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,5 +1,5 @@
 export TODOTXT_DEFAULT_ACTION=pv
-export BLOGROOT="$DEV/chronoslynx.github.com"
+export BLOGROOT="$DEV/chronoslynx.github.io"
 # export LANG=en_US.utf8
 # export LC_CTYPE=en_US.utf8
 
