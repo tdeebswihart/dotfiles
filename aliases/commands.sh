8ce1bb8615c42608170102f80dfb7f1d708d31d5
Tim Swihart <chronoslynx@gmail.com>
2013-04-16 07:50:11 -0400

    removing unused vim bundles, fiddling with 'notes'

--- aliases/commands.sh
+++ aliases/commands.sh
@@ -2,3 +2,4 @@ hash trash 2>/dev/null && alias rm=trash
 alias serve="python3 -m http.server 9494"
 alias timestamp='gawk "{now=strftime(\"%F %T \"); print now \$0; fflush(); }"'
 alias h='heroku'
+alias _='sudo'
