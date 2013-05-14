21ed57e0e70b7ae311f6d3d41908720eb3835234
Tim Swihart <timothy.swihart@gtri.gatech.edu>
2013-05-14 10:03:58 -0400

    added LC_ALL for linux

--- shell/exports.sh
+++ shell/exports.sh
@@ -2,4 +2,9 @@ export TODOTXT_DEFAULT_ACTION=pv
 export BLOGROOT="$DEV/chronoslynx.github.io"
 # export LANG=en_US.utf8
 # export LC_CTYPE=en_US.utf8
+case $(uname -s) in
+Linux)
+    export LC_ALL=en_US.UTF-8
+    ;;
+esac
 
