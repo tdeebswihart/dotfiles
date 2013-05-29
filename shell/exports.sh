c2080243e0391e7cc07ac88090bf63e87725e35b
Tim Swihart <timothy.swihart@gtri.gatech.edu>
2013-05-29 13:52:00 -0400

    adding history setups

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,5 +1,7 @@
 export TODOTXT_DEFAULT_ACTION=pv
 export BLOGROOT="$DEV/chronoslynx.github.io"
+export HISTFILE="$HOME/.zshitory"
+export HISTSIZE=10000
 # export LANG=en_US.utf8
 # export LC_CTYPE=en_US.utf8
 #case $(uname -s) in
