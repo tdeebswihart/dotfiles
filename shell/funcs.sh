b36cf9b95034cc6c21f575a7afa927a2715c035b
Tim Swihart <timothy.swihart@gtri.gatech.edu>
2013-07-18 09:20:53 -0400

    adding vim shell funcs

--- shell/funcs.sh
+++ shell/funcs.sh
@@ -10,4 +10,25 @@ hasteit() {
     esac
 }
 
+## VIM
+# Set the name of vim session the terminal is tied up to
+eset(){
+    export VI_SERVER=$1
+}
+
+# Fire up a new server according to the argument supplied
+vs(){
+    eset $1
+    vim --servername $VI_SERVER
+}
+
+# Open up the files in the environment Vim server.
+es(){
+    vim --servername $VI_SERVER --remote-silent $*
+}
+
+# Reuse Vim ZSH completions for vim completions
+compdef _vim es
+
+eset zshsession
 test -f ~/.local.sh && source ~/.local.sh
