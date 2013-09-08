299e05fcd2df9f1f70694b9324576d1400e39e4c
Tim Swihart <chronoslynx@gmail.com>
2013-09-08 10:41:49 +0100

    fixing rbenv for homebrew

--- shell/ruby.sh
+++ shell/ruby.sh
@@ -1,2 +1,3 @@
 export REALLY_GEM_UPDATE_SYSTEM=true
-[[ -s $HOME/.rbenv ]] && eval "$(rbenv init - --no-rehash)"
+[[ -s $HOME/.rbenv/bin ]] && eval "$($HOME/.rbenv/bin/rbenv init - --no-rehash)"
+[[ -s /usr/local/bin/rbenv ]] && eval "$(rbenv init - --no-rehash)"
