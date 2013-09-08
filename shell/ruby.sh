22709d1514a745c59cc207721482040f0f03ad02
Tim Swihart <chronoslynx@gmail.com>
2013-09-08 10:43:58 +0100

    .

--- shell/ruby.sh
+++ shell/ruby.sh
@@ -1,3 +1,3 @@
 export REALLY_GEM_UPDATE_SYSTEM=true
-[[ -s $HOME/.rbenv/bin ]] && eval "$($HOME/.rbenv/bin/rbenv init - --no-rehash)"
+[[ -d $HOME/.rbenv/bin ]] && eval "$($HOME/.rbenv/bin/rbenv init - --no-rehash)"
 [[ -s /usr/local/bin/rbenv ]] && eval "$(rbenv init - --no-rehash)"
