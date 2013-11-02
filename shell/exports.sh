bc1568dee919b07750ffbcdbe96b25f13ff155d7
Tim Swihart <chronoslynx@gmail.com>
2013-11-02 11:26:32 +0000

    fixing zsh history file

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,8 +1,13 @@
 export TODOTXT_DEFAULT_ACTION=pv
 export BLOGROOT="$DEV/chronoslynx.github.io"
-export HISTFILE="$HOME/.zshitory"
+export HISTFILE="$HOME/.zshistory"
 export HISTSIZE=10000
 export HASTE_SERVER="http://hastebin.com"
+
+for i in ls ~/.secret/*.sh; do
+    source $i
+done
+
 # export LANG=en_US.utf8
 # export LC_CTYPE=en_US.utf8
 #case $(uname -s) in
