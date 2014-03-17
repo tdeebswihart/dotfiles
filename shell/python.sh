4928f263f81c495ab11e52ade21da60d98c0a5a8
Tim Swihart <chronoslynx@gmail.com>
2014-03-17 09:02:44 -0700

    pyenv

--- shell/python.sh
+++ shell/python.sh
@@ -1,3 +1,5 @@
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 if test -s "$(which virtualenv)"; then
     export WORKON_HOME="$HOME/.snakepit"
