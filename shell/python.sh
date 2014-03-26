81fe33d0acaf3c8d09b6108bc983a81e4309cd2e
Tim Swihart <chronoslynx@gmail.com>
2014-03-26 13:40:01 -0400

    letting pyenv manage virtualenvs

--- shell/python.sh
+++ shell/python.sh
@@ -1,8 +1,10 @@
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+[[ -f "/usr/local/opt/autoenv/activate.sh" ]] && source "/usr/local/opt/autoenv/activate.sh"
 
-if test -s "$(which virtualenv)"; then
-    export WORKON_HOME="$HOME/.snakepit"
-    if [[ -e '/usr/local/bin/virtualenvwrapper.sh' ]]; then
-        source '/usr/local/bin/virtualenvwrapper.sh'
-    fi
-fi
+
+# if test -s "$(which virtualenv)"; then
+#     export WORKON_HOME="$HOME/.snakepit"
+#     if [[ -e '/usr/local/bin/virtualenvwrapper.sh' ]]; then
+#         source '/usr/local/bin/virtualenvwrapper.sh'
+#     fi
+# fi
