a3e3a88a711bd474025dfac90b712db7a34644ab
Tim Swihart <chronoslynx@gmail.com>
2014-03-27 09:52:05 -0400

    more aliases

--- aliases/commands.sh
+++ aliases/commands.sh
@@ -1,10 +1,10 @@
 hash trash 2>/dev/null && alias rm=trash
 alias wifi="networksetup -setairportpower en0"
-alias serve="python3 -mhttp.server "
-alias timestamp='gawk "{now=strftime(\"%F %T \"); print now \$0; fflush(); }" '
+alias serve="python3 -mhttp.server"
+alias timestamp='gawk "{now=strftime(\"%F %T \"); print now \$0; fflush(); }"'
 #alias h='heroku '
-alias _='sudo '
-alias jsonp='python -m json.tool '
+#alias _='sudo '
+alias jsonp='python -m json.tool'
 alias mounted='mount | column -t '
 alias asshole='echo You dont have to be rude, you know. && sleep 0.2 && sudo $(history -n -1) '
 alias sorry='sudo $(history -n -1) '
@@ -13,5 +13,5 @@ alias v='f -t -e vim -b viminfo'
 alias ijulia='ipython notebook --profile julia'
 alias ijl='ipython notebook --profile julia'
 alias ipy='ipython notebook'
-alias py=python
-alias rb=ruby
+alias py='python'
+alias rb='ruby'
