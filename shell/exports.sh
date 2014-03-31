c972378b040160dbb78ead996c9cffa5834112a1
Tim Swihart <chronoslynx@gmail.com>
2014-03-31 12:21:42 -0400

    .

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,6 +1,12 @@
 export TODOTXT_DEFAULT_ACTION=pv
+
+setopt APPEND_HISTORY
+setopt SHARE_HISTORY
+setopt HIST_IGNORE_DUPS
+
 export HISTFILE="$HOME/.zshistory"
 export HISTSIZE=10000
+export SAVEHIST=10000
 
 for i in `ls ~/.secret/*.sh`; do
     source $i
