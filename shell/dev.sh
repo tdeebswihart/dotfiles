dc573122cdbe5b4cd7de708928b99240ccf9d97b
Tim Swihart <chronoslynx@gmail.com>
2014-04-11 21:35:22 -0400

    .

--- shell/dev.sh
+++ shell/dev.sh
@@ -1,7 +1,7 @@
 export DEV=$HOME/Development
 
 #GO exports
-# export MYGO=$DEV/mygo
+export MYGO=$DEV/mygo/src/chronon
 # export GOROOT="$DEV/go"
 # export GOBIN="$GOROOT/bin"
 export GOPATH="$DEV/mygo"
