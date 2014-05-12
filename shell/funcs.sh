34f7a5a81353f836e9bf71f18df1b0cbb089a2f6
Tim Swihart <chronoslynx@gmail.com>
2014-05-12 18:18:51 -0400

    fixing >>>

--- shell/funcs.sh
+++ shell/funcs.sh
@@ -15,26 +15,10 @@ hilight () {
   $src | pygmentize -f rtf -O 'fontface=Inconsolata,style=tango'| sed 's;\\f0;\\f0\\fs60;g' | tr -d '\n' | sed 's;\\par}$;};' | pbcopy
 }
 
-<<<<<<< HEAD
-prettymd(){
+prettymd (){
     pandoc -t markdown --atx-headers --no-wrap $*
 }
-
-# Fire up a new server according to the argument supplied
-vs(){
-    eset $1
-    vim --servername $VI_SERVER
-}
-
-# Open up the files in the environment Vim server.
-es(){
-    vim --servername $VI_SERVER --remote-silent $*
-}
-
-gpx2json() {
-=======
 gpx2json () {
->>>>>>> 7f0ff044a9a841cc6e197165898b047ad0ffefa9
     while [ "$1" != "" ]; do
         ogr2ogr -f GeoJSON "${1}.json" "${1}" routes && shift
     done
