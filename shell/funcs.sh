8dabfc10d4bb493b3820b71b542ccadc9ecd8c67
Tim Swihart <chronoslynx@gmail.com>
2014-05-12 12:39:56 -0400

    .

--- shell/funcs.sh
+++ shell/funcs.sh
@@ -1,11 +1,11 @@
 ## VIM
 # Set the name of vim session the terminal is tied up to
-eset(){
+eset (){
     export VI_SERVER=$1
 }
 
 # Highlight code for use w/ keynote, etc
-hilight() {
+hilight () {
   if [ -z "$1" ]
     then src="pbpaste"
   else
@@ -15,25 +15,14 @@ hilight() {
   $src | pygmentize -f rtf -O 'fontface=Inconsolata,style=tango'| sed 's;\\f0;\\f0\\fs60;g' | tr -d '\n' | sed 's;\\par}$;};' | pbcopy
 }
 
-# Fire up a new server according to the argument supplied
-vs(){
-    eset $1
-    vim --servername $VI_SERVER
-}
-
-# Open up the files in the environment Vim server.
-es(){
-    vim --servername $VI_SERVER --remote-silent $*
-}
-
-gpx2json() {
+gpx2json () {
     while [ "$1" != "" ]; do
         ogr2ogr -f GeoJSON "${1}.json" "${1}" routes && shift
     done
 }
 
 # Reuse Vim ZSH completions for vim completions
-compdef _vim es
+which compdef && compdef _vim es
 
-eset zshsession
+which eset && eset zshsession
 test -f ~/.local.sh && source ~/.local.sh
