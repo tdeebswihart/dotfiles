d38d7cd4b8b12efb51623a462ba5c5f9a3c01556
Tim Swihart <chronoslynx@gmail.com>
2014-10-22 14:12:43 -0400

    emacsclient should spawn daemon if unavailable

--- shell/editor.sh
+++ shell/editor.sh
@@ -1,6 +1,7 @@
-alias e='emacsclient -t'
-alias ec='emacsclient -c'
-alias en='emacsclient -c -n'
+alias e='emacsclient -a "" -t'
+alias et='emacsclient -a "" -t'
+alias ec='emacsclient -a "" -c'
+alias en='emacsclient -a "" -c -n'
 export EDITOR=e
 export SUDOEDITOR=e
 export VISUAL=ec
