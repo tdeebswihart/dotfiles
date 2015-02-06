4aa77951266aa9bce10e879426b4e13e3289a5df
Tim Swihart <Timothy.Swihart@gtri.gatech.edu>
2015-02-06 13:36:41 -0500

    fixing pyenv setup

--- shell/python.sh
+++ shell/python.sh
@@ -1,6 +1,6 @@
 export PYENV_ROOT="$HOME/.pyenv"
-if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
-if which pyenv-virtualenv > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+test -d $PYENV_ROOT && eval "$(pyenv init -)"
+test -d $PYENV_ROOT/plugins/pyenv-virtualenv && eval "$(pyenv virtualenv-init -)"
 
 [[ -f "/usr/local/opt/autoenv/activate.sh" ]] && source "/usr/local/opt/autoenv/activate.sh"
 
