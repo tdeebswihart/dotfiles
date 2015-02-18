fa03337861a41d95fbfa0ccb6be3b7d5cdaace82
Tim Swihart <Timothy.Swihart@gtri.gatech.edu>
2015-02-18 09:37:20 -0500

    reorganizing

--- shell/tmux.sh
+++ shell/tmux.sh
@@ -1,2 +1,3 @@
 test "$TMUX" && export TERM="screen-256color" # rxvt-unicode-256color
 [[ -s $HOME/.tmuxinator/scripts/tmuxinator ]] && source $HOME/.tmuxinator/scripts/tmuxinator
+export DISABLE_AUTO_TITLE="true"
