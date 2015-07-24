6873be737426272669113ec9edb9097d1a95334a
Tim Swihart <Timothy.Swihart@gtri.gatech.edu>
2015-07-24 12:49:09 -0400

    in progress

--- emacs/config/global.el
+++ emacs/config/global.el
@@ -1,2 +1,126 @@
-(load-theme 'gruvbox)
+;; Requirements
+(require 'uniquify)
+
+;; Fundamental functions
+
+(defun delete-blank-lines-in (start end)
+  "Delete blank lines at point or in the region."
+  (interactive "r")
+  (replace-regexp "[\n]+" "\n" nil start end))
+
+(defun eval-replacing-region (read)
+  "Eval an expression on the region and replace the region with the
+  result."
+  (interactive "P")
+  (unless (region-active-p)
+    (error "Region is not active!"))
+  (let* ((string
+          (buffer-substring-no-properties
+           (region-beginning)
+           (region-end)))
+         (function
+          (eval
+           `(lambda (x)
+              ,(read-from-minibuffer "Expression on x: " "" nil t))))
+         (result (funcall function (if read (read string) string)))
+         (start (point)))
+    (delete-region (region-beginning)
+                   (region-end))
+    (insert (case (type-of result)
+              (string (format "%s" result))
+              (t (format "%S" result))))
+    (set-mark (point))
+    (goto-char start)))
+
+(defun unfill-paragraph ()
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive)
+  (let ((fill-column (point-max)))
+    (fill-paragraph nil)))
+
+(defun set-auto-saves ()
+  "Put autosave files (ie #foo#) in one place, *not*
+ scattered all over the file system!"
+  (defvar autosave-dir
+    (concat "/tmp/emacs_autosaves/" (user-login-name) "/"))
+
+  (make-directory autosave-dir t)
+
+  (defun auto-save-file-name-p (filename)
+    (string-match "^#.*#$" (file-name-nondirectory filename)))
+
+  (defun make-auto-save-file-name ()
+    (concat autosave-dir
+            (if buffer-file-name
+                (concat "#" (file-name-nondirectory buffer-file-name) "#")
+              (expand-file-name
+               (concat "#%" (buffer-name) "#")))))
+
+  (defvar backup-dir (concat "/tmp/emacs_backups/" (user-login-name) "/"))
+  (setq backup-directory-alist (list (cons "." backup-dir))))
+
+(defun auto-chmod ()
+  "If we're in a script buffer, then chmod +x that script."
+  (and (save-excursion
+         (save-restriction
+           (widen)
+           (goto-char (point-min))
+           (save-match-data
+             (looking-at "^#!"))))
+       (shell-command (concat "chmod u+x " buffer-file-name))
+       (message (concat "Saved as script: " buffer-file-name))))
+
+(defun insert-date ()
+  (interactive)
+  (insert (shell-command-to-string "date +'%Y-%m-%d'")))
+
+;; Global keybindings
+(global-set-key (kbd "C-c C-+") 'number/add)
+(global-set-key (kbd "C-c C--") 'number/sub)
+(global-set-key (kbd "C-c C-*") 'number/multiply)
+(global-set-key (kbd "C-c C-/") 'number/divide)
+(global-set-key (kbd "C-c C-0") 'number/pad)
+(global-set-key (kbd "C-c C-=") 'number/eval)
+(global-set-key (kbd "C-c C-:") 'eval-replacing-region)
+(global-set-key (kbd "C-x C-k C-o") 'delete-blank-lines-in)
+(global-set-key (kbd "M-g") 'goto-line)
+
+(global-set-key (kbd "<left>") 'windmove-left)
+(global-set-key (kbd "<right>") 'windmove-right)
+(global-set-key (kbd "<up>") 'windmove-up)
+(global-set-key (kbd "<down>") 'windmove-down)
+
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c M-x") 'execute-extended-command)
+;; Convenience bindings for god-mode
+
+(global-set-key (kbd "C->") 'end-of-buffer)
+(global-set-key (kbd "C-<") 'beginning-of-buffer)
+(global-set-key (kbd "C-!") 'eval-defun)
+
+;; Hooks
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
 (add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; Autoloads
+(add-to-list 'auto-mode-alist (cons "\\.el\\'" 'emacs-lisp-mode))
+(add-to-list 'auto-mode-alist (cons "\\.md\\'" 'markdown-mode))
+(add-to-list 'auto-mode-alist (cons "\\.markdown\\'" 'markdown-mode))
+
+;; Environment settings
+
+(set-language-environment "UTF-8")
+
+;; font and color
+(defun fontify-frame (frame)
+  (set-frame-parameter frame 'font "Input-18"))
+(fontify-frame nil)
+(push 'fontify-frame after-make-frame-functions)
+(load-theme 'gruvbox t)
+(global-smartscan-mode 1)
+
+
+
+(provide 'global)
--- emacs/init.el
+++ emacs/init.el
@@ -1,24 +1,26 @@
 ;; (Shamelessly based on chrisdone's emacs config)
 ;; Standard libraries needed
-(require 'cl)
+(add-to-list 'load-path "packages/dash")
+(require dash)
 
 ;; Packages to laod
 (defvar packages
   '(better-defaults
     company
     company-jedi
+    dash
     flycheck
     flycheck-pylama
     god-mode
     helm
     helm-projectile
-    magit
     markdown-mode
     projectile
     rainbow-delimeters
     rust-mode
     smartscan
     smartparens
+    smex
     sml-mode
     with-editor)
   "Packages whose location follows the
@@ -30,10 +32,25 @@
     "golang"
     "haskell"
     "markdown"
+    "my-magit"
     "rust"
     "python")
   "Configuration files that follow the config/foo.el path
   format.")
+
+(defvar custom-load-paths
+  '("emacs-async"
+    "helm"
+    "magit/lisp"))
+
+;; Add custom load paths
+(let (s)
+  (-each custom-load-paths
+    (lambda (loc) (add-to-list 'load-path
+                               (concat (file-name-directory (or load-file-name
+                                                                (buffer-file-name)))
+                                       "packages/"
+                                       location)))))
 
 ;; Load packages
 (loop for name in packages
@@ -44,19 +61,14 @@
                                        "packages/"
                                        (symbol-name name)))
                   (require name))))
+
 ;; Load configurations
 (loop for name in configs
       do (load (concat (file-name-directory load-file-name)
                        "config/"
                        name ".el")))
 
-;; font and color
-(defun fontify-frame (frame)
-  (set-frame-parameter frame 'font "Input-18"))
-(fontify-frame nil)
-(push 'fontify-frame after-make-frame-functions)
-(load-theme 'gruvbox t)
-(global-smartscan-mode 1)
 ;; Mode initializations
 (add-hook 'after-init-hook #'global-flycheck-mode)
+(smex-mode)
 (god-mode)
