9c3ed170c7168b0eccf448736ba414848df94738
Tim Swihart <Timothy.Swihart@gtri.gatech.edu>
2015-08-20 09:45:22 -0400

    rbenv and iterm titling

--- shell/funcs.sh
+++ shell/funcs.sh
@@ -150,4 +150,17 @@ function qcp () {
     fi
 }
 
+#setup terminal tab title
+function title {
+    if [ "$1" ]
+    then
+        unset PROMPT_COMMAND
+        echo -ne "\033]0;${*}\007"
+    else
+        export PROMPT_COMMAND='echo -ne "\033]0;${PWD/#$HOME/~}\007"'
+    fi
+}
+
+title
+
 test -f ~/.local.sh && source ~/.local.sh
