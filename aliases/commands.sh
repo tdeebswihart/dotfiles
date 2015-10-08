c70767bc72c32cd2495a92b7411a7865b7e6ec9e
Tim Swihart <chronoslynx@users.noreply.github.com>
2015-10-08 15:42:47 -0400

    adding support for thefuck

--- aliases/commands.sh
+++ aliases/commands.sh
@@ -16,3 +16,4 @@ alias aplay='ansible-playbook playbook.yml -i inventory'
 # Fix pyenv and homebrew *-config issues
 which brew > /dev/null 2>&1 && alias brew="env PATH=${PATH//$(pyenv root)\/shims:/} brew"
 which nvim >/dev/null 2>&1 && alias vim=nvim
+which thefuck >/dev/null && eval "$(thefuck --alias)"
