c2848fa12535cce1c2b6c401fa5fb94032f08b83
Tim Swihart <chronoslynx@users.noreply.github.com>
2016-07-26 10:37:46 -0400

    fix pyenv for windows

--- shell/python.sh
+++ shell/python.sh
@@ -3,6 +3,7 @@ export PYENV_ROOT="$HOME/.pyenv"
 if $(which pyenv 2>&1 >/dev/null); then
     eval "$(pyenv init --no-rehash -)"
 elif test -f $PYENV_ROOT/bin/pyenv; then
+    export PATH="$PATH:$PYENV_ROOT/bin"
     eval "$($PYENV_ROOT/bin/pyenv init --no-rehash -)"
 fi
 
