d98d088b4af21322f45d4793958f6333cd5a6338
Tim Swihart <chronoslynx@users.noreply.github.com>
2016-09-15 10:16:35 -0400

    fixing pyenv path

--- shell/python.sh
+++ shell/python.sh
@@ -1,10 +1,10 @@
 export PYENV_ROOT="$HOME/.pyenv"
 
+if test -f $PYENV_ROOT/bin/pyenv; then
+    export PATH="$PATH:$PYENV_ROOT/bin"
+fi
 if $(which pyenv 2>&1 >/dev/null); then
     eval "$(pyenv init --no-rehash -)"
-elif test -f $PYENV_ROOT/bin/pyenv; then
-    export PATH="$PATH:$PYENV_ROOT/bin"
-    eval "$($PYENV_ROOT/bin/pyenv init --no-rehash -)"
 fi
 
 test -d $PYENV_ROOT/plugins/pyenv-virtualenv && eval "$($PYENV_ROOT/bin/pyenv virtualenv-init -)"
