c23856ebe50e1f7fa742702db701f576ffe96a4b
Tim Swihart <chronoslynx@users.noreply.github.com>
2016-09-30 15:44:05 -0400

    remove pyenv

--- shell/python.sh
+++ shell/python.sh
@@ -1,12 +1,1 @@
-export PYENV_ROOT="$HOME/.pyenv"
-
-if test -f $PYENV_ROOT/bin/pyenv; then
-    export PATH="$PATH:$PYENV_ROOT/bin"
-fi
-if $(which pyenv 2>&1 >/dev/null); then
-    eval "$(pyenv init --no-rehash -)"
-fi
-
-test -d $PYENV_ROOT/plugins/pyenv-virtualenv && eval "$($PYENV_ROOT/bin/pyenv virtualenv-init -)"
-
 [[ -f "/usr/local/opt/autoenv/activate.sh" ]] && source "/usr/local/opt/autoenv/activate.sh"
