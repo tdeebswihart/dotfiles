7a3d3c1badeee5be635dbc66f5302fd356c54bae
Tim Swihart <chronoslynx@users.noreply.github.com>
2016-11-11 09:40:51 -0500

    giving pyenv another go

--- shell/python.sh
+++ shell/python.sh
@@ -1,1 +1,3 @@
 export VIRTUAL_ENV_DISABLE_PROMPT=1
+if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
