ed93af11ecb9a122efe967c375a9344518d7fb02
Tim Swihart <chronoslynx@users.noreply.github.com>
2017-09-03 01:05:28 -0400

    rm virtualenv

--- shell/python.sh
+++ shell/python.sh
@@ -1,5 +1,1 @@
 export VIRTUAL_ENV_DISABLE_PROMPT=1
-if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
-if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
-export WORKON_HOME="$HOME/.snakepit"
-which virtualenvwrapper.sh >/dev/null 2>&1 && source $(which virtualenvwrapper.sh)
