225f8448842275881efe0bba42461c79709bf861
Tim Swihart <chronoslynx@users.noreply.github.com>
2017-09-10 08:36:37 -0400

    fix ignoring

--- shell/funcs.sh
+++ shell/funcs.sh
@@ -179,17 +179,19 @@ function serve () {
 
 #Quick CoPy
 function qcp () {
-    IGNORE_FILES=(~/.gitignore ./.gitignore ./.rsyncignore)
     if [ $# -lt 2 ]; then
         echo "usage: qcp [rsync flags] source dest"
     else
+        IGNORE_FILES=($HOME/.gitignore ./.gitignore ./.rsyncignore)
         EXCLUDE_FROM=""
+        ARGS=$*
         for f in ${IGNORE_FILES[@]}; do
           if [[ -e $f ]]; then
-            EXCLUDE_FROM="$EXCLUDE_FROM --exclude-from=$f "
+            EXCLUDE_FROM="$EXCLUDE_FROM --exclude-from=\"$f\" "
           fi
         done
-        rsync -azhW $EXCLUDE_FROM $*
+        # I don't know why it doesn't work if I run it directly...
+        bash -c "$(echo rsync -azhW $EXCLUDE_FROM $ARGS)"
     fi
 }
 
