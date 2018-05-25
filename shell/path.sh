a358120529c9f95b22ae72fa55bfe80dbe986808
Tim Swihart <chronoslynx@users.noreply.github.com>
2018-05-25 18:23:02 -0400

    simplify adding cruft to my path

--- shell/path.sh
+++ shell/path.sh
@@ -1,36 +1,33 @@
+__maybe_append () {
+  # Add each argument to basepath (our first argument) if it exists
+    test -z "$1" && echo ""
+    local basepath="$1"
+    shift
+    while test ${#} -gt 0
+    do
+        local candidate="$1"
+        if [[ -d "$candidate" ]]; then
+          basepath="$candidate:$basepath"
+        fi
+        shift
+    done
+    echo "$basepath"
+}
+
 case $(uname -s) in
     Linux)
         # /usr/lib/lightdm/lightdm:
         BASEPATH="$PATH"
-        hash keychain && eval 'keychain --eval id_rsa'
+        #hash keychain && eval 'keychain --eval id_rsa'
         alias md5='md5sum'
         ;;
     Darwin)
         # since /usr/local is managed by homebrew (and it complains about "unmanaged" pkgs, I use $HOME/.local
         BASEPATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:"
-        if [[ -d "/opt/X11/bin" ]]; then
-          BASEPATH="$BASEPATH:/opt/X11/bin"
-        fi
-        if [[ -d "/usr/local/MacGPG2/bin" ]]; then
-          PATH="$BASEPATH:/usr/local/MacGPG2/bin"
-        fi
+        BASEPATH=$(__maybe_append "$BASEPATH" "/opt/X11/bin" "/Library/TeX/texbin" "$HOME/macports/bin")
+
         if [ -z "$JAVA_HOME" -a -d /System/Library/Frameworks/JavaVM.framework/Home ]; then
             export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
-        fi
-
-        # NPM for node
-        if [[ -d "/usr/local/share/npm/bin" ]]; then
-            BASEPATH="$BASEPATH:/usr/local/share/npm/bin"
-        fi
-
-        # haskell
-        if [[ -d "$HOME/Library/Haskell/bin" ]]; then
-            BASEPATH="$HOME/Library/Haskell/bin:$BASEPATH"
-        fi
-
-        if [[ -d "/Library/TeX/texbin" ]]; then
-            # Fix for TeX on El Capitan
-            BASEPATH="/Library/TeX/texbin:$BASEPATH"
         fi
 
         # GDAL data file locations
@@ -41,36 +38,16 @@ case $(uname -s) in
         # OPAM configuration
         hash opam &>/dev/null && . $HOME/.opam/opam-init/init.zsh > /dev/null 2> /dev/null || true
 
-        # Haskell, since the brew installed version has issues
-        # Add GHC 7.8.4 to the PATH, via https://ghcformacosx.github.io/
-        export GHC_DOT_APP="$HOME/Applications/ghc-7.8.4.app"
-        if [ -d "$GHC_DOT_APP" ]; then
-            BASEPATH="${HOME}/.cabal/bin:${GHC_DOT_APP}/Contents/bin:${BASEPATH}"
-        fi
-
         # iTerm 2.9+
         test -f "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
-
-        # MacPorts
-        if [[ -d "$HOME/macports/bin" ]]; then
-          BASEPATH="$HOME/macports/bin:$BASEPATH"
-        fi
-        if [[ -d "$HOME/miniconda3/bin" ]]; then
-          BASEPATH="$HOME/miniconda3/bin:$BASEPATH"
-        fi
 esac
 
-# Cabal binfiles
-if [[ -d "$HOME/.cabal/bin" ]]; then
-    BASEPATH="$HOME/.cabal/bin:$BASEPATH"
-fi
-
-if [[ -d "$GOPATH/bin" ]]; then
-    BASEPATH="$GOPATH/bin:$BASEPATH"
-fi
-
-if [[ -d "$HOME/.cargo/bin" ]]; then
-    BASEPATH="$HOME/.cargo/bin:$BASEPATH"
+BASEPATH=$(__maybe_append "$BASEPATH" "$HOME/.cabal/bin" "$HOME/.cargo/bin" "$HOME/miniconda3/bin" "$HOME/anaconda3/bin")
+if [[ ! -z "$GOPATH" ]]; then
+    if [[ -d "$GOPATH/bin" ]]; then
+        # Only add go it if GOPATH is nonempty
+        BASEPATH="$GOPATH/bin:$BASEPATH"
+    fi
 fi
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
