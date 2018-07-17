a209b886712a8d6031b39420c93024f4f074c635
Tim Swihart <chronoslynx@users.noreply.github.com>
2018-07-17 08:53:46 -0400

    Define a custom histdb file

--- shell/history.sh
+++ shell/history.sh
@@ -1,5 +1,6 @@
 if test -d "$HOME/.zsh-histdb"; then
   source "$HOME/.zsh-histdb/sqlite-history.zsh"
+  export HISTDB_FILE="$HOME/.zsh-history.db"
   autoload -Uz add-zsh-hook
   add-zsh-hook precmd  histdb-update-outcome
 fi
