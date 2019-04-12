8ed8ca5449a7d6277962ffc80e60047147f34ebf
Tim Deeb-Swihart <tdeeb-swihart@bishopfox.com>
2019-04-12 11:33:22 -0700

    fix prompt expansion

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -135,6 +135,13 @@ prompt_virtualenv() {
   fi
 }
 
+prompt_git() {
+  local branch=$(g branch -q --color=never 2>/dev/null| grep '^\*' | cut -d ' ' -f2)
+  if [[ ! -z "$branch" ]]; then
+    prompt_segment blue white "git:$branch"
+  fi
+}
+
 RUST_DEFAULT=$(test -f ~/.rustup/settings.toml && (grep default_toolchain ~/.rustup/settings.toml | cut -d '=' -f2 | tr -d ' "\n'))
 
 prompt_rustenv() {
@@ -169,11 +176,13 @@ prompt_status() {
 build_prompt() {
   RETVAL=$?
   prompt_status
-  #prompt_virtualenv
-  prompt_whoami
-  prompt_codenv
   prompt_dir
+  #prompt_virtualenv
+  #prompt_whoami
+  prompt_codenv
+  prompt_git
   prompt_end
 }
 
-PROMPT="%{%f%b%k%}$(build_prompt) "
+setopt PROMPT_SUBST
+PROMPT='%{%f%b%k%}$(build_prompt) '
