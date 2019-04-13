134f1b84f6d149e6fa235347e226b6e4e6e5cc68
Tim Deeb-Swihart <tim@deebswih.art>
2019-04-13 16:16:14 -0400

    I dont want to fix it

--- zsh/1.sh
+++ zsh/1.sh
@@ -2,13 +2,3 @@
 [ -z "$PS1" ] && return
 test -d "$HOME/homebrew/bin" && export PATH="$HOME/homebrew/sbin:$HOME/homebrew/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
 
-#Replaces ... -> ../.. on the fly
-function rationalise-dot {
-    if [[ $LBUFFER = *..  ]]; then
-        LBUFFER+=/..
-    else
-        LBUFFER+=.
-    fi
-}
-zle -N rationalise-dot
-bindkey . rationalise-dot
