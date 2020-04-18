c59b5093ab5213533ee453595938cecb4e35547a
Tim Deeb-Swihart <tim@deebswih.art>
2020-04-18 13:00:08 -0400

    v

--- hammerspoon/usb.lua
+++ hammerspoon/usb.lua
@@ -2,12 +2,24 @@ local module = {}
 
 local evtAdd = "added"
 local evtRemove = "removed"
+local fiion = "FiiO USB DAC-E17"
+local bluen = "Blue Snowball "
 
 function usbDeviceCallback(data)
    -- TODO set audio output to the FiiO DAC
    -- TODO set audio input to the blue snowball
    pn = data["productName"]
    evt = data["eventType"]
+   if pn == blue then
+     if evt == evtAdd then
+       hs.findDeviceByName(blue):setDefaultInputDevice()
+     end
+   elseif pn == fiio then
+     if evt == evtAdd then
+       hs.findDeviceByName(fiio):setDefaultOutputDevice()
+     end
+   end
+
 end
 
 module.watcher = hs.usb.watcher.new(usbDeviceCallback)
