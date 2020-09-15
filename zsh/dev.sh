805a30c425f3afea74b382a52e40e00fb4bbf43a
Tim Deeb-Swihart <chronoslynx@users.noreply.github.com>
2020-09-15 18:37:19 -0400

    TIMEFMT is fantastic

--- zsh/dev.sh
+++ zsh/dev.sh
@@ -16,3 +16,17 @@ export CARGO_HOME="$HOME/.cargo"
 export CHOOSENIM_NO_ANALYTICS=1
 export DESTDIR="$HOME/.local"
 which direnv >/dev/null && eval "$(direnv hook zsh)"
+
+if [[ `uname` == Darwin ]]; then
+    MAX_MEMORY_UNITS=KB
+else
+    MAX_MEMORY_UNITS=MB
+fi
+
+export TIMEFMT='%J   %U  user %S system %P cpu %*E total'$'\n'\
+'avg shared (code):         %X KB'$'\n'\
+'avg unshared (data/stack): %D KB'$'\n'\
+'total (sum):               %K KB'$'\n'\
+'max memory:                %M '$MAX_MEMORY_UNITS''$'\n'\
+'page faults from disk:     %F'$'\n'\
+'other page faults:         %R'
